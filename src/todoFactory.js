6fa3904e8c6db9b8b7ba98450f34674dbf48c3af
Justy Oberg <justyoberg1@gmail.com>
2022-12-29 15:46:43 -0800

    Create HTML and some CSS styling to get the page layout ready

---
+++ src/todoFactory.js
@@ -0,0 +1,3 @@
+export default function createTodo(formObj) {
+  // TODO Create a function to take a formObject and turn it into a TODO item.
+}
--- webpack.config.js
+++ webpack.config.js
@@ -7,12 +7,4 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
   mode: "development",
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
 };
